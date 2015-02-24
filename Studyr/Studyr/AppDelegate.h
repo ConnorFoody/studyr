db613db8da1b53e6db129bd81fc2fe5c9c531d65
Jon Patsenker <jpatsenker@dcc-wl-462.dynamic2.rpi.edu>
2015-02-24 16:23:36 -0500

    iOS project added

---
+++ Studyr/Studyr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Studyr
+//
+//  Created by Jon Patsenker on 2/24/15.
+//  Copyright (c) 2015 Jonathan Patsenker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
