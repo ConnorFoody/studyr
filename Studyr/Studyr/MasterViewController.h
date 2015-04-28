242e56e3b0a4511d6a41a85aa1dcb049424e56b1
ConnorFoody <connorfoody@gmail.com>
2015-04-28 13:36:00 -0400

    Revert a5dfe77..8995e6c
    This rolls back to commit a5dfe77ba668fa7a8e1f6f006c58b9e56a843d6e.

---
+++ Studyr/Studyr/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Studyr
+//
+//  Created by Jon Patsenker on 2/24/15.
+//  Copyright (c) 2015 Jonathan Patsenker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
