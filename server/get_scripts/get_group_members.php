ff0e634b64aa637736af2a13f0651c19e5c3cac9
ConnorFoody <connorfoody@gmail.com>
2015-05-04 20:28:40 -0400

    Changed a script on the back end

---
+++ server/get_scripts/get_group_members.php
@@ -0,0 +1,12 @@
+// <?php
+// 	include '../studyrlib.php';
+
+// 	$db = new Studyr();
+// 	if(!$_GET['group']){
+// 		echo json_encode(["ERROR: incorrect parameters]");
+// 	}
+// 	else{
+// 		//$member_ids = $db->getGroupMembers($_GET['group']);
+// 		$group = $db->getGroupAsJson($_GET['group']);
+// 		echo json_encode($group);
+// ?>
